de47fcf32de332fb1c9dd1eb22b8a2d434ad2c30
Ozxoch <120763000+Ozxoch@users.noreply.github.com>
2024-02-23 01:10:14 -0500

    Create ttt.cpp

---
+++ ttt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "ttt_functions.hpp"
+
+int main (){
+	  welcome();
+    setUp();
+    showGrid();
+    play();
+    endGame();
+}
