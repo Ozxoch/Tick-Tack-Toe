af5f7c509adf7370e78c80991f994a2455685c31
Ozxoch <120763000+Ozxoch@users.noreply.github.com>
2024-02-23 01:03:18 -0500

    Create ttt.cpp
    main cpp

---
+++ ttt.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <vector>
+#include <string>
+
+void welcome();
+void setUp();
+void showGrid();
+bool isWinner();
+std::string getMark();
+int getPlayer();
+void play();
+void endGame();
+
